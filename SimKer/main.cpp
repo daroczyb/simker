ecd0b8a654bc469f0fea97b420a7b422c1f6c076
daroczyb <daroczyb@gmail.com>
2015-01-07 21:16:10 +0100

    Initial Commit

---
+++ SimKer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimKer
+//
+//  Created by Daroczy Balint on 07/01/2015.
+//  Copyright (c) 2015 Daroczy Balint. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
